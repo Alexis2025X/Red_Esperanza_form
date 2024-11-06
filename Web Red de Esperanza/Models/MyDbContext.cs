d5e4faa27bd4f756ffce7b18061e9b879c8a4a38
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-11-06 15:33:45 -0600

    Ya se puede hacer un CRUD

--- Web Red de Esperanza/Models/MyDbContext.cs
+++ Web Red de Esperanza/Models/MyDbContext.cs
@@ -18,6 +18,9 @@ namespace Web_Red_de_Esperanza.Models
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
         //NECESITADOS
         public DbSet<Necesitado> necesitados { get; set; }
+        
+        //vista
+       // public DbSet<View_Necesitado> View_necesitados { get; set; }
         //DESAPARECIDOS
         public DbSet<Desaparecidos> desaparecidos { get; set; }
         //CUENTAS
@@ -27,6 +30,8 @@ namespace Web_Red_de_Esperanza.Models
         {  //NECESITADOS
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Necesitado>().HasKey(e => e.Id_publicacionNese);
+            
+            //modelBuilder.Entity<View_Necesitado>().HasKey(e => e.Id_publicacionNese);
             //DESAPARECIDOS
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Desaparecidos>().HasKey(e => e.Id_publicacionDesa);
@@ -35,7 +40,7 @@ namespace Web_Red_de_Esperanza.Models
             modelBuilder.Entity<cuentas>().HasKey(e => e.Id_cuenta);
         }
 
-
+      
     }
 
 
--- Web Red de Esperanza/Models/necesitados.cs
+++ Web Red de Esperanza/Models/necesitados.cs
@@ -11,14 +11,15 @@ namespace Web_Red_de_Esperanza.Models
         public required string Apellido { get; set; }
         public int Edad { get; set; }
         public required string Direccion { get; set; }
-        public int Distrito_id { get; set; }
-        public int Ayuda_id { get; set; }
+        public int distrito_id { get; set; }
+        public int ayuda_id { get; set; }
         public required string Situacion_descripcion { get; set; }
         public DateTime Fecha_publicacion { get; set; }
-        public int Asistido_id { get; set; }
+        public int asistido_id { get; set; }
         public string? Telefono { get; set; }
         public string? WhatsApp { get; set; }
-        public int Publicado_por { get; set; }
+        public int publicado_por { get; set; }
+       // public string name { get; set; }
         //public int Imagen_necesitados { get; set; }
 
     }
